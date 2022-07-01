b8b5bd652fe80ebed15a4314b6ac2907fdf11969
keem2000 <joycelinaomari@gmail.com>
2022-06-30 20:13:55 -0700

    i

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 * main - prints to string
 * Return: 1
 */
-int main(void) 
+int main(void)
 {
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
@@ -12,8 +12,8 @@ long syscall = 1;
 long ret = 0;
 __asm__ ("syscall"
 : "=a" (ret)
-: "a" (syscall),	     
-"D" (fd),	     
+: "a" (syscall),
+"D" (fd),
 "S" (s),
 "d" (l));
 return (1);
