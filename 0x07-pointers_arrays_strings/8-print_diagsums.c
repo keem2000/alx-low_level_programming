f8e39558e13197fafe65b29ff6b9060ae22e0495
keem2000 <joycelinaomari@gmail.com>
2022-07-18 18:57:15 -0700

    7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,18 +8,13 @@
 */
 void print_diagsums(int *a, int size)
 {
-/*Declaring variables*/
-int i;
-int sum1, sum2;
-sum1 = 0;
-sum2 = 0;
-i = 0;
-while (i < size) /*number repetitions*/
+int i, sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
 {
-/*sums for diagsums*/
-sum1 = sum1 + *(a + i * size + i);
-sum2 = sum2 + *(a + i * size + size - i - 1);
-i++; /*add +1*/
+sum1 += a[i];
+sum2 += a[size - i - 1];
+a += size;
 }
-printf("%i, %i\n", sum1, sum2);
+printf("%d, ", sum1);
+printf("%d\n", sum2);
 }
